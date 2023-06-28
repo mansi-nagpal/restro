8810362c12fa7d5019f8d0accde9a72a0698a6d8
mansi-nagpal <134012873+mansi-nagpal@users.noreply.github.com>
2023-06-28 23:50:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const app=express();
+app.use(express.static('public'));
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/header.html");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("hi")
+})
